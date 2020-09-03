1edc9fb8b06a834669dc1cb78d1b1264b3f575a6
GilbertWillian <gilbertowillian99@gmail.com>
2020-09-03 14:41:07 -0300

    Preparando a estrutura base do projeto

---
+++ src/Conversor-moedas.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Conversor-moedas.css';
+
+function ConversorMoedas() {
+  return (
+    <h1>Conversor de moedas</h1>
+  );
+}
+
+export default ConversorMoedas;
